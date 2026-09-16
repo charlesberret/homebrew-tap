c21d4b00ed52f794cc593e62ee8e05cb332d7faa
Charles Berret <charles.berret@gmail.com>
2026-09-16 15:58:06 -0400

    cowbell is MIT; declare it in the formula

--- Formula/cowbell.rb
+++ Formula/cowbell.rb
@@ -5,6 +5,7 @@ class Cowbell < Formula
   homepage "https://pypi.org/project/cowbell/"
   url "https://files.pythonhosted.org/packages/e8/73/a31062655d6102076e80f2b7181d85304209da665eed023ad87470b7402f/cowbell-0.3.0.tar.gz"
   sha256 "aba7a2535f7b8e05c809b73cfe5c118be775acdb1f7fb47c9d24f8a0187e374b"
+  license "MIT"
 
   depends_on "python@3.13"
 
